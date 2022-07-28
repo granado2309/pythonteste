f078081f87f2749f325fcfe292d47ca6e145313a
Everton Granado Silva <granado2309@gmail.com>
2022-07-28 08:24:13 -0300

    Initial commit

---
+++ aula007b.py
@@ -0,0 +1,5 @@
+n1 = int(input('Escreva um número: '))
+s = n1+1
+ant = n1-1
+print('número sucessor de {} é {} e o seu antecessor  é {}'.format(n1,s,ant))
+
---
+++ aula007c.py
@@ -0,0 +1,8 @@
+n1 = int(input('Escreva um número: '))
+do = n1 * 2
+trip = n1 * 3
+r = float(n1)**0.5
+print('dobro de número {}  é  {} e o triplo  é {} a raiz é {}' .format(n1,do,trip,r))
+
+
+
