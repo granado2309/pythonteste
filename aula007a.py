7d81598448a185eb5a7297c81f39c5d04c47ddc6
Everton Granado Silva <granado2309@gmail.com>
2022-07-28 07:04:34 -0300

    Initial commit

---
+++ aula007a.py
@@ -0,0 +1,11 @@
+n1 = int(input('Dígite um número: '))
+n2 = int(input('outro valor: '))
+s = n1+n2
+m = n1*n2
+d = n1/n2
+di= n1//n2
+e = n1**n2
+print(' soma vale {}, multi é {}, divisão é {:.3f}, divisão inteira é {}'.format(s,m,d,di))
+print('A edição é {}'.format(e))
+
+
---
+++ aula06b.py
@@ -0,0 +1,10 @@
+n1 = int(input(' Primeiro número '))
+n2 = int(input(' Segundo número  '))
+soma = n1 + n2
+
+print ( 'A soma de {} + {}  é igual {}'.format(n1,n2,soma ) )
+
+
+
+
+
